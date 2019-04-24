ec7f76e8d525e0b2a120ccdbef093926857bf19f
Neo <echiang73@gmail.com>
2019-04-24 13:44:49 -0700

    Updated logic so consistent and takes in survey correctly and display in api/superheroes. Need to do logic to match next

--- app/data/superheroesData.js
+++ app/data/superheroesData.js
@@ -1,19 +1,14 @@
 var profileArray = [
     {
-      name: "Eddie Chiang",
-      superheroName: "Neo",
-      photoLink: "#",
-      q1: "1",
-      q2: "1",
-      q3: "1",
-      q4: "1",
-      q5: "1",
-      q6: "1",
-      q7: "1",
-      q8: "1",
-      q9: "1",
-      q10: "1"
-    }
-  ];
+      "userName": "Eddie Chiang",
+      "superHeroName": "Neo",
+      "userPhoto": "#",
+      "score": [1,1,1,1,1,1,1,1,1,1]
+    },{
+      "userName": "Eddie Chiang2",
+      "superHeroName": "Neo2",
+      "userPhoto": "#",
+      "score": [2,2,2,2,2,2,2,2,2,2]
+    }];
   
   module.exports = profileArray;
